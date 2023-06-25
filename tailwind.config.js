2cd6ef4f057266ed47a68683a798ac7830b5c0ad
Rufi <68145072+Rufi-git@users.noreply.github.com>
2023-06-25 08:48:24 -0700

    sadasdsadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+module.exports = {
+  purge: {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      defaultExtractor: (content) => {
+        // Specify the output file path
+        return content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+      },
+    },
+  },
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
